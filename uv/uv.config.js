9f71908e5de53bb1e425bee1700de6ae48551f60
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2023-10-10 19:44:21 -0500

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -3,8 +3,8 @@ self.__uv$config = {
     bare: 'https://ensuremath.com/api/bare/',
     encodeUrl: Ultraviolet.codec.base64.encode,
     decodeUrl: Ultraviolet.codec.base64.decode,
-    handler: '/uv/uv.handler.js',
-    bundle: '/uv/uv.bundle.js',
-    config: '/uv/uv.config.js',
-    sw: '/uv/uv.sw.js',
+    handler: 'https://cdn.jsdelivr.net/gh/DoxrGitHub/uvhost/uv/uv.handler.js',
+    bundle: 'https://cdn.jsdelivr.net/gh/DoxrGitHub/uvhost/uv/uv.bundle.js',
+    config: 'https://cdn.jsdelivr.net/gh/DoxrGitHub/uvhost/uv/uv.config.js',
+    sw: 'https://cdn.jsdelivr.net/gh/DoxrGitHub/uvhost/uv/uv.sw.js',
 };
